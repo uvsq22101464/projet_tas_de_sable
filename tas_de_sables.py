ef071487448c23b8d68e9f4623d86ed94ed20f5a
Ethan <ethan.mache@gmail.com>
2022-02-15 12:50:30 +0100

    Grille

--- tas_de_sables.py
+++ tas_de_sables.py
@@ -16,22 +16,27 @@ import random
 #########################################################
 #Variables globales :
 WIDTH, HEIGHT = 500, 500
-
+N = 3
+grille = []
 
 #########################################################
 #Fonctions :
 def configuration_courante() :
-    pass
-
+    for i in range(N) :
+        grille.append([])
+        for j in range(N):
+            grille[i].append(random.randint(0,10))
+    print(grille)
+#création de la grille
 
 #########################################################
 #Widgets :
 racine = tk.Tk()
 racine.title("tas de sable")
-canvas = tk.Canvas(racine, width=WIDTH, height=HEIGHT, bg='white')
-bouton_01 = tk.Button(racine, text="configuration aléatoire")
+canvas = tk.Canvas(racine, width=WIDTH, height=HEIGHT, bg='black')
+bouton_01 = tk.Button(racine, text="configuration aléatoire", command=configuration_courante)
 
-canvas.create_text((250,250), text="salut", )
+#canvas.create_text((250,250), text="salut", font=("helvetica", 50))
 
 #########################################################
 #Emplacements des widgets :
@@ -41,4 +46,5 @@ bouton_01.grid(row=1)
 
 #########################################################
 #le reste quoi :
+
 racine.mainloop()
