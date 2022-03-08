f98379c91338fcba9b19d60dd71b55f6ec6b33dc
Ethan <ethan.mache@gmail.com>
2022-03-08 13:08:29 +0100

    '-'

--- tas_de_sables.py
+++ tas_de_sables.py
@@ -20,10 +20,8 @@ N = 10
 grille = []
 espacementHauteur = HEIGHT / N
 espacementLargeur = WIDTH / N
-centrageLargeur = espacementLargeur / 2
-centrageHauteur = espacementHauteur / 2
-taillePixelLargeur = WIDTH / N / N * 1.5
-taillePixelHauteur = HEIGHT / N / N * 1.5
+taillePixelLargeur = WIDTH / N
+taillePixelHauteur = HEIGHT / N
 
 liste_couleurs = []
 
@@ -31,6 +29,7 @@ liste_couleurs = []
 #Fonctions :
 
 def configuration_courante() :
+    grille, liste_couleurs = [], []
     for i in range(N) :
         grille.append([])
         liste_couleurs.append([])
@@ -45,10 +44,25 @@ def configuration_courante() :
             else :
                 liste_couleurs[i].append("red")
             couleur = liste_couleurs[i][j]
-            canvas.create_rectangle(espacementLargeur * i + centrageLargeur, espacementHauteur * j + centrageHauteur, \
-                espacementLargeur * i + centrageLargeur + taillePixelLargeur, espacementHauteur * j + centrageHauteur + taillePixelHauteur, fill=couleur)
+            canvas.create_rectangle(espacementLargeur * i, espacementHauteur * j, \
+                espacementLargeur * i + taillePixelLargeur, espacementHauteur * j + taillePixelHauteur, fill=couleur)
     print(grille)
-#création de la grille
+
+def config_vide() :
+    grille, liste_couleurs = [], []
+    for i in range(N) :
+        grille.append([])
+        liste_couleurs.append([])
+        for j in range(N):
+            grille[i].append(0)
+            liste_couleurs[i].append("white")
+            couleur = liste_couleurs[i][j]
+            canvas.create_rectangle(espacementLargeur * i, espacementHauteur * j, \
+                espacementLargeur * i + taillePixelLargeur, espacementHauteur * j + taillePixelHauteur, fill=couleur)
+    print(grille)
+
+#def calcul() :
+
 
 #########################################################
 #Widgets :
@@ -56,14 +70,17 @@ racine = tk.Tk()
 racine.title("tas de sable")
 canvas = tk.Canvas(racine, width=WIDTH, height=HEIGHT, bg='black')
 bouton_01 = tk.Button(racine, text="configuration aléatoire", command=configuration_courante)
+bouton_02 = tk.Button(racine, text="config vide", command=config_vide)
+bouton_03 = tk.Button(racine, text="ajouter 0 à 3 grains de sable")
 
 #canvas.create_text((250,250), text="salut", font=("helvetica", 50))
 
 #########################################################
 #Emplacements des widgets :
-canvas.grid()
-bouton_01.grid(row=1)
-
+canvas.grid(row=0, column=0, columnspan=3)
+bouton_01.grid(row=1, column=0)
+bouton_02.grid(row=1, column=1)
+bouton_03.grid(row=1, column=2)
 
 #########################################################
 #le reste quoi :
